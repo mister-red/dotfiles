84cb447359068f63a194cff295dd1d5d9dfe01e7
Daniel Carty <carty.dj@gmail.com>
2026-02-16 12:02:23 -0600

    updating obsidian plugin to function properly

--- .config/nvim/lua/plugins/obsidian.lua
+++ .config/nvim/lua/plugins/obsidian.lua
@@ -1,99 +1,16 @@
 return {
   "obsidian-nvim/obsidian.nvim",
-  version = "*", -- recommended, use latest release instead of latest commit
-  lazy = true,
+  version = "*", -- use latest release, remove to use latest commit
   ft = "markdown",
-  keys = {
-    {
-      "<leader>nn",
-      function()
-        require("obsidian").util.toggle_checkbox()
-      end,
-      desc = "Notes: Toggle checkbox",
-      mode = "n",
-    },
-    { "<leader>no", ":ObsidianOpen<cr>", desc = "Notes: Open in Obsidian", mode = "n" },
-    { "<leader>ns", ":ObsidianSearch<cr>", desc = "Notes: Search", mode = "n" },
-    { "<leader>nt", ":ObsidianTags<cr>", desc = "Notes: Tags", mode = "n" },
-    { "<leader>nd", ":ObsidianToday<cr>", desc = "Notes: Daily note", mode = "n" },
-  },
-  -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-  -- event = {
-  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
-  --   "BufReadPre path/to/my-vault/**.md",
-  --   "BufNewFile path/to/my-vault/**.md",
-  -- },
-  dependencies = {
-    -- Required.
-    "nvim-lua/plenary.nvim",
-    "hrsh7th/nvim-cmp",
-    "nvim-telescope/telescope.nvim",
-    "nvim-treesitter/nvim-treesitter",
-    "epwalsh/pomo.nvim",
-  },
   ---@module 'obsidian'
   ---@type obsidian.config
   opts = {
-    ui = {
-      enable = false,
-    },
+    legacy_commands = false, -- this will be removed in the next major release
     workspaces = {
       {
         name = "red",
-        path = vim.fn.expand("~/garden/red"),
+        path = "~/garden",
       },
     },
-    completion = {
-      nvim_cmp = true,
-      -- Trigger completion at 2 chars.
-      min_chars = 2,
-      --  * "current_dir" - put new notes in same directory as the current buffer.
-      --  * "notes_subdir" - put new notes in the default notes subdirectory.
-      -- 1. Whether to add the note ID during completion.
-      -- E.g. "[[Foo" completes to "[[foo|Foo]]" assuming "foo" is the ID of the note.
-    },
-
-    wiki_link_func = function(opts)
-      if opts.id == nil then
-        return string.format("[[%s]]", opts.label)
-      elseif opts.label ~= opts.id then
-        return string.format("[[%s|%s]]", opts.id, opts.label)
-      else
-        return string.format("[[%s]]", opts.id)
-      end
-    end,
-
-    notes_subdir = "0. Inbox",
-    new_notes_location = "notes_subdir",
-
-    disable_frontmatter = true,
-    note_frontmatter_func = function(note)
-      -- This is equivalent to the default frontmatter function.
-      local out = {
-        id = note.id,
-        aliases = note.aliases,
-        tags = note.tags,
-        area = "",
-        project = "",
-      }
-
-      -- `note.metadata` contains any manually added fields in the frontmatter.
-      -- So here we just make sure those fields are kept in the frontmatter.
-      if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
-        for k, v in pairs(note.metadata) do
-          out[k] = v
-        end
-      end
-      return out
-    end,
-
-    templates = {
-      subdir = "Templates",
-      date_format = "%Y-%m-%d",
-      time_format = "%H:%M:%S",
-      substitutions = {},
-    },
-    -- see below for full list of options 👇
   },
 }
